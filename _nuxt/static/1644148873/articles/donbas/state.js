4077b531c815c0bbc364a43a4cf8b09c599b5911
rubennaatje <ruben.soerdien@hotmail.com>
2022-02-06 12:01:20 +0000

    Deploying to gh-pages from @ front-lines-org/content-test@2afe6d898d8ef7c6045816fc5674bb0fbcb326df 🚀

---
+++ _nuxt/static/1644148873/articles/donbas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644148873",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fdonbas",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a2cd7d85"}}}}(null));
