b37d2e4f89945a753571051635da7d2e44efdeb8
rubennaatje <ruben.soerdien@hotmail.com>
2022-02-12 20:27:51 +0000

    Deploying to gh-pages from @ front-lines-org/content-test@301e12d32bc85a346eb95b52fa7e4b94c5184bc3 🚀

---
+++ _nuxt/static/1644697664/articles/russian_retaliation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644697664",layout:"article-layout",error:a,serverRendered:true,routePath:"\u002Farticles\u002Frussian_retaliation",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f7cdcf58"}}}}(null));
