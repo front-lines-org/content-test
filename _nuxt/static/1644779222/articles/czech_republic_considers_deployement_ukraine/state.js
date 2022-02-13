f346747ba359f7e7e77b0b950f41988b15f479f9
rubennaatje <ruben.soerdien@hotmail.com>
2022-02-13 19:07:09 +0000

    Deploying to gh-pages from @ front-lines-org/content-test@94bf3b60f4ec21dc2a568fb61938ec2368fda0df 🚀

---
+++ _nuxt/static/1644779222/articles/czech_republic_considers_deployement_ukraine/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644779222",layout:"article-layout",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fczech_republic_considers_deployement_ukraine",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"65466f16"}}}}(null));
