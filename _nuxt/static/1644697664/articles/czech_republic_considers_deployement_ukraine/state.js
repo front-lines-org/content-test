25a5c24d3a77b1290ccbe27206ff493fb8e7699f
rubennaatje <ruben.soerdien@hotmail.com>
2022-02-13 16:36:29 +0000

    Deploying to gh-pages from @ front-lines-org/content-test@7c405219d3384864d4bcd2c873ebd8f9499297e0 🚀

--- _nuxt/static/1644697664/articles/czech_republic_considers_deployement_ukraine/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644697664",layout:"article-layout",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fczech_republic_considers_deployement_ukraine",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f7cdcf58"}}}}(null));
--- _nuxt/static/1644697664/articles/russia_invasion/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644697664",layout:"article-layout",error:a,serverRendered:true,routePath:"\u002Farticles\u002Frussia_invasion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f7cdcf58"}}}}(null));
