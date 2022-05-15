8db50f0a539bbed0a17a5c0826a8e1a6d01c0d1b
rubennaatje <ruben.soerdien@hotmail.com>
2022-05-15 21:01:25 +0000

    Deploying to gh-pages from @ front-lines-org/content-test@eb09dc6e6c944b79b5e175e575703a352a68ef12 🚀

--- _nuxt/static/1652624820/articles/russia_invasion/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652624820",layout:"article-layout",error:a,serverRendered:true,routePath:"\u002Farticles\u002Frussia_invasion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5e9408c0"}}}}(null));
---
+++ _nuxt/static/1652648474/Reports/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652648474",layout:"default",error:a,serverRendered:true,routePath:"\u002FReports",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"09c7413f"}}}}(null));
