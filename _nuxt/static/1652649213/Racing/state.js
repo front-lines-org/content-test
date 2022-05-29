b0aca7500dca0035af23e2149804dd2da9d15512
rubennaatje <ruben.soerdien@hotmail.com>
2022-05-29 09:39:13 +0000

    Deploying to gh-pages from @ front-lines-org/content-test@38f547cad0a0b63bd68596d93f5ecde1258a8000 🚀

--- _nuxt/static/1652649213/Racing/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652649213",layout:"default",error:a,serverRendered:true,routePath:"\u002FRacing",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aa5f6657"}}}}(null));
--- _nuxt/static/1652649213/articles/Ukrainian_retake_airfield_from_seperatist/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652649213",layout:"article-layout",error:a,serverRendered:true,routePath:"\u002Farticles\u002FUkrainian_retake_airfield_from_seperatist",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aa5f6657"}}}}(null));
